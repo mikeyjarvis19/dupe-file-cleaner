286fb7d8766633bed9b2f6b14df2cb022c9c2092
Mike <michaeljarvis95@gmail.com>
2020-08-11 12:48:22 +0100

    Add support for cli args with fire

--- dupes.py
+++ dupes.py
@@ -1,10 +1,7 @@
 import pathlib
 import re
+import fire
 
-starting_directory = ""
-starting_path = pathlib.Path(starting_directory)
-files_found = []
-paths_to_clean = []
 regex_pattern = r".+\(\d\)"
 regex = re.compile(regex_pattern)
 
@@ -15,18 +12,22 @@ def is_dupe_filename(filename):
     result = regex.match(filename)
     return result is not None
 
-extensions = ["jpg", "png"]
-for extension in extensions:
-    for path in starting_path.rglob(f'*.{extension}'):
-        files_found.append(path)
+def look_for_dupes(starting_directory):
+    starting_path = pathlib.Path(starting_directory)
+    total_filezise = 0
+    extensions = ["jpg", "png"]
+    paths_to_clean = []
+    for extension in extensions:
+        for path in starting_path.rglob(f'*.{extension}'):
+            if is_dupe_filename(str(path)):
+                filesize_bytes = path.stat().st_size
+                filesize_mb = bytes_to_mb(filesize_bytes)
+                total_filezise += filesize_mb
+                paths_to_clean.append(path)
+                print(path)
+    
+    print(f"Total filesize: {total_filezise}MB")
 
-total_filezise = 0
-for path in files_found:
-    if is_dupe_filename(str(path)):
-        filesize_bytes = path.stat().st_size
-        filesize_mb = bytes_to_mb(filesize_bytes)
-        total_filezise += filesize_mb
-        paths_to_clean.append(path)
-        print(path)
-
-print(f"Total filesize: {total_filezise}MB")
+if __name__ == '__main__':
+    # Example command: `python dupes.py --starting_directory "E:\Pictures"`
+  fire.Fire(look_for_dupes)
